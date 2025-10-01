8ade4d21362f0bf950b7c8b76906067068f4a77e
Rusafid Ahamed <rusafidt@yahoo.com>
2025-10-01 17:28:19 +0100

    Added frontend and backend

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import os
 import nltk
 from nltk.corpus import movie_reviews
 import random
@@ -152,4 +153,5 @@ async def demo():
     return {"demo_results": results}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
